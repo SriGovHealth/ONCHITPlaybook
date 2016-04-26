8af66a2bd0fcd61b008f5bef2459f7bb1e1dfdec
Jason Morris <jasonmorris@gmail.com>
2016-04-26 16:12:57 -0400

    Fix active nav state when scrolling back up

--- js/lib/onc.js
+++ js/lib/onc.js
@@ -6,10 +6,13 @@ $(document).scroll(function () {
     var top = window.pageYOffset;
     var distance = top - $(this).offset().top;
     var hash = $(this).attr('id');
+    var path = window.location.pathname + window.location.hash;
+    var prevHash = $(this).prevAll('.section-header:first').attr('id');
+
     if (distance < 30 && distance > -30 && currentHash !== hash) {
       history.replaceState({page: 1}, hash, "#" + hash);
       currentHash = hash;
-      var path = window.location.pathname + window.location.hash;
+      path = window.location.pathname + window.location.hash;
 
       $('.subnav li a').each(function(){
         var myHref = $(this).attr('href');
@@ -19,16 +22,30 @@ $(document).scroll(function () {
           $(this).addClass("now");
         }
       });
+    }
 
-      if (path.indexOf(topHash) > -1) {
-        //$('.has-subnav').hasClass("active").addClass("current");
-        $('.has-subnav').each(function(){
-          if ($(this).hasClass('active')) {
-            $(this).addClass('current');
+    else if (distance < -30 && currentHash === hash) {
+      if (typeof prevHash !== 'undefined') {
+        path = window.location.pathname + "#" + prevHash;
+
+        $('.subnav li a').each(function(){
+          var myHref = $(this).attr('href');
+          if (path === myHref) {
+            $('.has-subnav').removeClass("current");
+            $('.subnav li a').removeClass("now");
+            $(this).addClass("now");
           }
         });
-        $('.subnav li a').removeClass("now");
       }
+    }
+
+    if (path.indexOf(topHash) > -1) {
+      $('.has-subnav').each(function(){
+        if ($(this).hasClass('active')) {
+          $(this).addClass('current');
+        }
+      });
+      $('.subnav li a').removeClass("now");
     }
   });
 });
