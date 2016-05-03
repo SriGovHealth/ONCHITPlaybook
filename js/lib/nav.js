c66fa74fb74f85a785845d182945086eeea6be28
Jason Morris <jasonmorris@gmail.com>
2016-05-02 23:01:42 -0400

    Replace aria-expanded with aria-hidden for sidebar

--- js/lib/nav.js
+++ js/lib/nav.js
@@ -4,33 +4,33 @@ $(".site-menu button").click(function (event) {
 
   if ($(".sidebar").hasClass("closed")) {
     $(".sidebar").removeClass("closed");
-    $(".sidebar").addClass("open").attr("aria-expanded", "true");
+    $(".sidebar").addClass("open").attr("aria-hidden", "false");
     $(".overlay").addClass("on");
   }
   else {
     $(".sidebar").removeClass("open");
-    $(".sidebar").addClass("closed").attr("aria-expanded", "false");
+    $(".sidebar").addClass("closed").attr("aria-hidden", "true");
     $(".overlay").removeClass("on");
   }
 });
 
 $(".overlay").click(function (event) {
   $(".sidebar").removeClass("open");
-  $(".sidebar").addClass("closed").attr("aria-expanded", "false");
+  $(".sidebar").addClass("closed").attr("aria-hidden", "true");
   $(".overlay").removeClass("on");
 });
 
 $(".subnav a").click(function (event) {
   $(".sidebar").removeClass("open");
-  $(".sidebar").addClass("closed").attr("aria-expanded", "false");
+  $(".sidebar").addClass("closed").attr("aria-hidden", "true");
   $(".overlay").removeClass("on");
 });
 
 
 // Open subnav of active page
 if ($(".group").hasClass("active")) {
-  $(".group.active").children("ul .subnav").removeClass("closed").attr("aria-hidden", "false").attr("aria-expanded", "true");
-  $(".group.active").children("a.subnav-toggle").html("&rarr;");
+  $(".group.active").children("ul .subnav").removeClass("closed").attr("aria-hidden", "false");
+  $(".group.active").children("a.subnav-toggle").html("&rarr;").attr("aria-expanded", "true");
 }
 
 // Open/close subnavsw
@@ -40,17 +40,17 @@ $(".subnav-toggle").click(function (event) {
 
   if ($(this).parent().find(".subnav").hasClass("closed")) {
     // reset other nav links
-    $(".subnav").addClass("closed").attr("aria-hidden", "true").attr("aria-expanded", "false");
+    $(".subnav").addClass("closed").attr("aria-hidden", "true");
     $(".has-subnav").removeClass("active");
-    $(".subnav-toggle").html("&darr;");
+    $(".subnav-toggle").html("&darr;").attr("aria-expanded", "false");
 
-    $(this).parent().find(".subnav").removeClass("closed").attr("aria-hidden", "false").attr("aria-expanded", "true");
-    $(this).html("&uarr;");
+    $(this).parent().find(".subnav").removeClass("closed").attr("aria-hidden", "false");
+    $(this).html("&uarr;").attr("aria-expanded", "true");
     $(this).parent().addClass("active");
   }
   else {
-    $(this).parent().find(".subnav").addClass("closed").attr("aria-hidden", "true").attr("aria-expanded", "false");
-    $(this).html("&darr;");
+    $(this).parent().find(".subnav").addClass("closed").attr("aria-hidden", "true");
+    $(this).html("&darr;").attr("aria-expanded", "false");
     $(this).parent().removeClass("active");
   }
 });
@@ -59,11 +59,11 @@ $(".subnav-toggle").click(function (event) {
 if (window.matchMedia) {
   if (window.matchMedia("(min-width: 56.25em)").matches) {
     $(".mobile-menu").attr("aria-hidden", "true");
-    $(".sidebar").attr("aria-expanded", "true");
+    $(".sidebar").attr("aria-hidden", "false");
   }
   if (window.matchMedia("(max-width: 56.25em)").matches) {
     $(".mobile-menu").attr("aria-hidden", "false");
-    $(".sidebar").attr("aria-expanded", "false");
+    $(".sidebar").attr("aria-hidden", "true");
   }
 }
 
