a045bb8ce44bc3d7d7449a30e121fe6ca46bd0c0
Jason Morris <jasonmorris@gmail.com>
2016-05-05 14:51:55 -0400

    IG 1 progress

--- js/lib/infographics.js
+++ js/lib/infographics.js
@@ -1,21 +1,10 @@
-$(".js-btn-next").click(function (event) {
+$(".ig-nav").click(function (event) {
   if (event.preventDefault) { event.preventDefault(); }
   else { event.returnValue = false; } // IE
 
-  var next = $(this).attr('href');
-  if ($(next).hasClass("off")) {
-    $(".slide").addClass("off");
-    $(next).removeClass("off");
-  }
-});
-
-$(".js-btn-previous").click(function (event) {
-  if (event.preventDefault) { event.preventDefault(); }
-  else { event.returnValue = false; } // IE
-
-  var previous = $(this).attr('href');
-  if ($(previous).hasClass("off")) {
-    $(".slide").addClass("off");
-    $(previous).removeClass("off");
+  var target = $(this).attr('href');
+  if ($(target).hasClass("off")) {
+    $(".slide").addClass("off").attr("aria-hidden", "true").attr("tabindex", "");
+    $(target).removeClass("off").attr("aria-hidden", "false");
   }
 });
