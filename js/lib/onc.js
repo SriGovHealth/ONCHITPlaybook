492c09aa19ba07a5e8ed1fb734d8303b1dd4cda1
Jason Morris <jasonmorris@gmail.com>
2016-05-05 20:14:41 -0400

    Update screenshot slideshows
    - Navigation independent of slides
    - Navigation buttons self-referencing

--- js/lib/onc.js
+++ js/lib/onc.js
@@ -50,21 +50,51 @@ $(document).scroll(function () {
   });
 });
 
-$(".content-slide-next").click(function (event) {
-  //if (event.preventDefault) { event.preventDefault(); }
-  //else { event.returnValue = false; } // IE
-  var next = $(this).attr('href');
-  $(".content-slide").removeClass("on").attr("aria-hidden", "true");
-  $(next).addClass("on").attr("aria-hidden", "false");
+
+
+$(".content-slide-prev").click(function (event) {
+  if (event.preventDefault) { event.preventDefault(); }
+  else { event.returnValue = false; } // IE
+
+  var prevItem = $(".content-slide.on").prev();
+
+  $(".content-slide-prev").removeClass("limit");
+  $(".content-slide-next").removeClass("limit");
+
+  if(!$(".content-slide.on").prev('.content-slide').prev('.content-slide').length) {
+    $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
+    $(prevItem).addClass("on").attr("aria-hidden", "false");
+    $(".content-slide-prev").addClass("limit");
+  }
+  else {
+    $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
+    $(prevItem).addClass("on").attr("aria-hidden", "false");
+    $(".content-slide-prev").removeClass("limit");
+  }
 });
 
-$(".content-slide-previous").click(function (event) {
-  //if (event.preventDefault) { event.preventDefault(); }
-  //else { event.returnValue = false; } // IE
-  var previous = $(this).attr('href');
-  $(".content-slide").removeClass("on").attr("aria-hidden", "true");
-  $(previous).addClass("on").attr("aria-hidden", "false");
+$(".content-slide-next").click(function (event) {
+  if (event.preventDefault) { event.preventDefault(); }
+  else { event.returnValue = false; } // IE
+
+  var nextItem = $(".content-slide.on").next();
+
+  $(".content-slide-prev").removeClass("limit");
+  $(".content-slide-next").removeClass("limit");
+
+  if(!$(".content-slide.on").next('.content-slide').next('.content-slide').length) {
+    $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
+    $(nextItem).addClass("on").attr("aria-hidden", "false");
+    $(".content-slide-next").addClass("limit");
+  }
+  else {
+    $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
+    $(nextItem).addClass("on").attr("aria-hidden", "false");
+    $(".content-slide-next").removeClass("limit");
+  }
 });
+
+
 
 $(".reality-control").click(function (event) {
   if (event.preventDefault) { event.preventDefault(); }
