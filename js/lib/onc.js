b986caf03799540c65d512f1e746ca55002f7381
Jason Morris <jasonmorris@gmail.com>
2016-05-09 13:35:01 -0400

    Add 'start over' button, background light blue

--- js/lib/onc.js
+++ js/lib/onc.js
@@ -60,6 +60,7 @@ $(".content-slide-prev").click(function (event) {
 
   $(".content-slide-prev").removeClass("limit");
   $(".content-slide-next").removeClass("limit");
+  $(".content-slide-restart").addClass("limit");
 
   if(!$(".content-slide.on").prev('.content-slide').prev('.content-slide').length) {
     $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
@@ -81,18 +82,36 @@ $(".content-slide-next").click(function (event) {
 
   $(".content-slide-prev").removeClass("limit");
   $(".content-slide-next").removeClass("limit");
+  $(".content-slide-restart").addClass("limit");
 
   if(!$(".content-slide.on").next('.content-slide').next('.content-slide').length) {
     $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
     $(nextItem).addClass("on").attr("aria-hidden", "false");
     $(".content-slide-next").addClass("limit");
+    $(".content-slide-restart").removeClass("limit");
   }
   else {
     $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
     $(nextItem).addClass("on").attr("aria-hidden", "false");
     $(".content-slide-next").removeClass("limit");
+    $(".content-slide-restart").addClass("limit");
   }
 });
+
+$(".content-slide-restart").click(function (event) {
+  if (event.preventDefault) { event.preventDefault(); }
+  else { event.returnValue = false; } // IE
+
+  $(".content-slide-prev").addClass("limit");
+  $(".content-slide-next").removeClass("limit");
+  $(".content-slide-restart").addClass("limit");
+
+  $(".content-slide.on").removeClass("on").attr("aria-hidden", "true");
+  $("#content-slide-1").addClass("on").attr("aria-hidden", "false");
+
+});
+
+
 
 $(".sticky-header").stick_in_parent();
 
