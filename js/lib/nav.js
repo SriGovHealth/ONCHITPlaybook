519e5f8bca33e5e0acbd0ba76805202d793e5521
Mike Godin <mikegodin@yahoo.com>
2016-05-20 12:48:29 -0400

    Set menu to display:none when closed.

--- js/lib/nav.js
+++ js/lib/nav.js
@@ -1,15 +1,35 @@
+var $sidebar = $(".sidebar");
+
+function removeClosedFromSidebar() {
+  $sidebar.removeClass("closed");
+}
+
+function removeClosedFromSidebarDelayed() {
+  setTimeout(removeClosedFromSidebar,10);
+}
+
+function AddVeryClosedToSidebar() {
+  $sidebar.addClass("veryClosed");
+}
+
+function AddVeryClosedToSidebarDelayed() {
+  setTimeout(AddVeryClosedToSidebar,150);
+}
+
 $(".site-menu button").click(function (event) {
   if (event.preventDefault) { event.preventDefault(); }
   else { event.returnValue = false; } // IE
 
   if ($(".sidebar").hasClass("closed")) {
-    $(".sidebar").removeClass("closed");
+    $(".sidebar").removeClass("veryClosed");
+    removeClosedFromSidebarDelayed();
     $(".sidebar").addClass("open").attr("aria-hidden", "false");
     $(".overlay").addClass("on");
   }
   else {
     $(".sidebar").removeClass("open");
     $(".sidebar").addClass("closed").attr("aria-hidden", "true");
+    AddVeryClosedToSidebarDelayed();
     $(".overlay").removeClass("on");
   }
 });
@@ -17,12 +37,14 @@ $(".site-menu button").click(function (event) {
 $(".overlay").click(function (event) {
   $(".sidebar").removeClass("open");
   $(".sidebar").addClass("closed").attr("aria-hidden", "true");
+   AddVeryClosedToSidebarDelayed();
   $(".overlay").removeClass("on");
 });
 
 $(".subnav a").click(function (event) {
   $(".sidebar").removeClass("open");
   $(".sidebar").addClass("closed").attr("aria-hidden", "true");
+   AddVeryClosedToSidebarDelayed();
   $(".overlay").removeClass("on");
 });
 
@@ -69,6 +91,16 @@ if (window.matchMedia) {
     $(".sidebar").attr("aria-hidden", "true");
   }
 }
+
+window.onresize = function(){
+  if (window.innerWidth >= 960 || !$(".sidebar").hasClass("closed") ) { 
+    $(".mobile-menu").attr("aria-hidden", "true");
+    $(".sidebar").attr("aria-hidden", "false");
+  } else {
+    $(".mobile-menu").attr("aria-hidden", "false");
+    $(".sidebar").attr("aria-hidden", "true");
+  }
+};
 
 // Keep active nav item visible on load
 $(document).ready(function(){
